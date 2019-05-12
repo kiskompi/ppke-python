d14a4062fccee2bc32cf429a039c1200deb6fb5e
kiskompi <kiskompi@gmail.com>
2019-05-12 14:05:52 +0200

    error fixes. it runs now again

--- run.py
+++ run.py
@@ -3,20 +3,19 @@ from parameters import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def show(qmaze):
     plt.grid(True)
-    nrows, ncols = qmaze.maze.shape
+    nrows, ncols = qmaze.m_maze.shape
     ax = plt.gca()
     ax.set_xticks(np.arange(0.5, nrows, 1))
     ax.set_yticks(np.arange(0.5, ncols, 1))
     ax.set_xticklabels([])
     ax.set_yticklabels([])
-    canvas = np.copy(qmaze.maze)
-    for row,col in qmaze.visited:
+    canvas = np.copy(qmaze.m_maze)
+    for row,col in qmaze.m_visited:
         canvas[row,col] = 0.6
-    rat_row, rat_col, _ = qmaze.state
-    canvas[rat_row, rat_col] = 0.3   # rat cell
+    agent_row, agent_col, _ = qmaze.m_state
+    canvas[agent_row, agent_col] = 0.3   # agent cell
     canvas[nrows-1, ncols-1] = 0.9 # cheese cell
     img = plt.imshow(canvas, interpolation='none', cmap='gray')
     return img
@@ -35,5 +34,5 @@ maze = [
 qmaze = Qmaze(maze)
 canvas, reward, game_over = qmaze.act(DOWN)
 print("reward=", reward)
-plt.show(qmaze)
+show(qmaze)
 
